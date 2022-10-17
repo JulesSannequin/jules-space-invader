791589e3ae00d4fe10462ae3106d4b0c8f4dd9a1
Jules Sannequin <jules.sannequin@gmail.com>
2022-10-17 19:27:38 +0200

    create the invaders and make then move in the game

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ world.width = world.clientWidth;
 world.height = world.clientHeight;
 
 let frames = 0;
-const missiles = [];
 
 const keys = {
   ArrowLeft: { pressed: false },
@@ -73,6 +72,63 @@ class Player {
   }
 }
 
+class Alien {
+  constructor({ position }) {
+    this.velocity = { x: 0, y: 0 };
+    const image = new Image();
+    image.src = "./ghost.png";
+    image.onload = () => {
+      this.image = image;
+      this.width = 35;
+      this.height = 35;
+      this.position = {
+        x: position.x,
+        y: position.y,
+      };
+    };
+  }
+
+  draw() {
+    if (this.image) {
+      c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  update({ velocity }) {
+    if (this.image) {
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
+      if (this.position.y + this.height >= world.height) {
+        console.log("you loose bro 👽");
+      }
+    }
+    this.draw();
+  }
+
+  shoot(alienMissiles) {
+    if (this.position) {
+      alienMissiles.push(
+        new alienMissiles({
+          position: {
+            x: this.position.x,
+            y: this.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: 3,
+          },
+        })
+      );
+    }
+  }
+}
+
 class Missile {
   constructor({ position }) {
     this.position = position;
@@ -92,22 +148,71 @@ class Missile {
   }
 }
 
+class Grid {
+  constructor() {
+    this.position = { x: 0, y: 0 };
+    this.velocity = { x: 0.5, y: 0 };
+    this.invaders = [];
+    // let rows = Math.floor((world.height / 36) * (1 / 3));
+    // const colums = Math.floor((world.width / 36) * (2 / 3));
+    this.height = rows * 36;
+    this.width = colums * 36;
+    for (let x = 0; x < colums; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Alien({
+            position: {
+              x: x * 36,
+              y: y * 36,
+            },
+          })
+        );
+      }
+    }
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y = 0;
+    if (this.position.x + this.width >= world.width || this.position.x == 0) {
+      this.velocity.x = this.velocity.x;
+      this.velocity.y = 34;
+    }
+  }
+}
+
+const missiles = [];
+
+let grids = [new Grid()];
+
 const player = new Player();
 
+let particules = [];
+
 const animationLoop = () => {
-  requestAnimationFrame(animationLoop);
   c.clearRect(0, 0, world.width, world.height);
   //   console.log(frames);
   player.update();
+  requestAnimationFrame(animationLoop);
+
   missiles.forEach((missile, index) => {
     if (missile.position.y + missile.height <= 0) {
       setTimeout(() => {
         missiles.splice(index, 1);
-      });
+      }, 0);
     } else {
       missile.update();
     }
   });
+
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update({ velocity: grid.velocity });
+    });
+  });
+
   frames++;
 };
 animationLoop();
